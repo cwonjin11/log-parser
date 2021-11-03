1c4c4c848176587593249685b2b5455fe11d3650
Wonjin Cho <cwonjin1@gmail.com>
2021-11-03 11:47:30 -0400

    small changes

---
+++ app/controllers/parsers_controller.rb
@@ -0,0 +1,21 @@
+
+require 'pry'
+
+
+class ParsersController < ApplicationController
+
+
+    def index 
+        
+    end
+      
+
+
+      
+        # @result = @file.split(/<\d+>/)
+        # File.foreach(ARGV.first) { |line| Event.new(line.chomp) if line.start_with?(/<\d+>/) }
+
+
+
+
+end
