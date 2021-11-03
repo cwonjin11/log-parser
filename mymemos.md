5b04e5c9a1815e820611201f0de4ea259625aaf8
Wonjin Cho <cwonjin1@gmail.com>
2021-11-03 12:16:33 -0400

    Add button to get back from the parsed data show page

--- mymemos.md
+++ mymemos.md
@@ -9,3 +9,101 @@
         <% end %>
 
 </div> -->
+
+
+require 'pry'
+require 'json'
+class EventsController < ApplicationController
+
+    before_action :find_event, only: [:show, :destroy]
+
+    def index
+
+
+        @events = Event.all 
+#         # binding.pry
+#         @@all_events = [] 
+
+#         file = File.open("sample_data.txt", "r")
+#         while (line = file.gets)
+#             @@all_events << line
+#         end
+#         file.close
+#        @events =  @@all_events.map do |event| event.gsub(/,/, "<br />") #if event.start_with?(/<\d+>/)
+#         # binding.pry
+# # binding.pry
+#         end
+    
+
+    end
+    def show
+
+    # parsing source IP address
+    
+    @srcIP = @event.data.match(/src=(?<source_ip>(?:\d{1,3}\.){3}\d{1,3})/)
+    @dstIP = @event.data.match(/dst=(?<destination_ip>(?:\d{1,3}\.){3}\d{1,3})/)
+    # binding.pry
+    # parsing Destination IP address
+
+
+    end
+
+    def new
+
+        
+        @event = Event.new
+        
+        # @file = File.open("sample_data.txt", "a")
+        # # binding.pry
+    end
+
+    def create
+        # binding.pry
+
+        @event = Event.new(event_params)
+
+        if @event.save
+            flash[:message] = "You've successfully created a new event."
+            redirect_to events_path(@event) #directly send the user to the review page 
+        else
+            render :new
+        end
+
+        # path = "sample_data.txt"
+        # # binding.pry
+        # content = @file
+        # File.open(path, "w+") do |f|
+        #   f.write(content)
+          
+        # end
+        
+        # if @file.save
+        #     redirect_to event_index_path(@file)
+        # else 
+        #     redirect_to new_event_path
+        #     flash[:message] = "something wrong"
+        # end
+
+    end
+
+
+    def destroy
+        @event.destroy
+        flash[:message] = "You've successfully deleted the event!"
+        redirect_to events_path
+
+    end
+
+
+    private
+
+        def find_event
+            @event= Event.find_by_id(params[:id])
+        end
+
+        def event_params
+            params.require(:event).permit(:data) 
+        end     
+    
+
+end
--- sample_data copy.rb
+++ sample_data copy.rb
@@ -1,14 +1,4 @@
-<37>CEF:0|TippingPoint|UnityOne|1.0.0.17|7610|Adlumin RepDV Manual Block|1|app=IP
-cnt=3 dst=52.10.98.6 dpt=443 act=Block cn1=0 cn1Label=VLAN ID cn2=33554431
-cn2Label=Taxonomy cn3=0 cn3Label=Packet Trace cs1=WCU-External-Outbound
-cs1Label=Profile Name cs2=6e664408-b90a-48e2-9a2d-c01cb9258381 cs2Label=Policy UUID
-cs3=00000001-0001-0001-0001-000000007610 cs3Label=Signature UUID cs4=1-1B 1-1A
-cs4Label=ZoneNames cs5=TipSMS cs5Label=SMS Name dvchost=PAS-TIP2600NX-01
-cs6=50.227.44.198 cs6Label=Filter Message Parms src=50.227.44.198 spt=10162
-externalId=19278229 rt=1539348361056 cat=Reputation proto=IP deviceInboundInterface=3
-c6a2= c6a2Label=Source IPv6 c6a3= c6a3Label=Destination IPv6 request= requestMethod=
-dhost= sourceTranslatedAddress=50.227.44.198 c6a1= c6a1Label=Client IPv6 suser=
-sntdom= duser= dntdom=
+<37>CEF:0|TippingPoint|UnityOne|1.0.0.17|7610|Adlumin RepDV Manual Block|1|app=IP cnt=3 dst=52.10.98.6 dpt=443 act=Block cn1=0 cn1Label=VLAN ID cn2=33554431 cn2Label=Taxonomy cn3=0 cn3Label=Packet Trace cs1=WCU-External-Outbound cs1Label=Profile Name cs2=6e664408-b90a-48e2-9a2d-c01cb9258381 cs2Label=Policy UUID cs3=00000001-0001-0001-0001-000000007610 cs3Label=Signature UUID cs4=1-1B 1-1A cs4Label=ZoneNames cs5=TipSMS cs5Label=SMS Name dvchost=PAS-TIP2600NX-01 cs6=50.227.44.198 cs6Label=Filter Message Parms src=50.227.44.198 spt=10162 externalId=19278229 rt=1539348361056 cat=Reputation proto=IP deviceInboundInterface=3 c6a2= c6a2Label=Source IPv6 c6a3= c6a3Label=Destination IPv6 request= requestMethod= dhost= sourceTranslatedAddress=50.227.44.198 c6a1= c6a1Label=Client IPv6 suser= sntdom= duser= dntdom=
 
 <35>CEF:0|TippingPoint|UnityOne|1.0.0.17|13012|13012: SIP: SipVicious Brute Force SIP Tool|7|app=TCP cnt=1 dst=72.44.211.10 dpt=5060 act=Block cn1=0 cn1Label=VLAN ID cn2=16929789 cn2Label=Taxonomy cn3=0 cn3Label=Packet Trace cs1=WCU-External-Inbound cs1Label=Profile Name cs2=00000002-0002-0002-0002-000000013012 cs2Label=Policy UUID cs3=00000001-0001-0001-0001-000000013012 cs3Label=Signature UUID cs4=6A 6B cs4Label=ZoneNames cs5=TipSMS cs5Label=SMS Name dvchost=Phoenix-1400N-1 cs6= cs6Label=Filter Message Parms src=37.49.231.177 spt=5067 externalId=19277953 rt=1539343940073 cat=Exploits proto=TCP deviceInboundInterface=11 c6a2= c6a2Label=Source IPv6 c6a3= c6a3Label=Destination IPv6 request= requestMethod= dhost= sourceTranslatedAddress=37.49.231.177 c6a1= c6a1Label=Client IPv6 suser= sntdom= duser= dntdom=
 
