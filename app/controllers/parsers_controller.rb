5e991dbd7bff37c7a5875418c6133b91ce092ae4
Wonjin Cho <cwonjin1@gmail.com>
2021-11-03 13:34:55 -0400

    Refactor Regex match method for both srcIP and dstIP

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -1,12 +1,21 @@
 
 require 'pry'
-
+require 'ipaddress'
 
 class ParsersController < ApplicationController
 
 
     def index 
-        
+        @events = Event.all
+        # binding.pry
+        # ip = IPAddress::IPv4.new(ip)
+        # IPAddress.valid? ip
+
+
+        # parsing source IP address
+        # @srcIP = @event.data.match(/src=(?<source_ip>(?:\d{1,3}\.){3}\d{1,3})/)
+        # parsing Destination IP address
+        # @dstIP = @event.data.match(/dst=(?<destination_ip>(?:\d{1,3}\.){3}\d{1,3})/)
     end
       
 
