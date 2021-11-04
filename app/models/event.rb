3fa4c207b5a577f4203ef43cf07d2ea92053e4a6
Wonjin Cho <cwonjin1@gmail.com>
2021-11-04 13:40:15 -0400

    add comment

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 
 class Event< ApplicationRecord
-
-    has_many :comments, dependent: :destroy
-
     
+    has_many :comments, dependent: :destroy
+    # dependent: :destroy means the comments related
+    #to the specific post in mention get deleted if the post does
+    
+    # log creation time formatting
     def datetime
         self.created_at.strftime("%Y-%m-%d / %H:%M:%S") if self.created_at
     end
