1168e669aea595286847d8f36289426c950e25bb
Wonjin Cho <cwonjin1@gmail.com>
2021-11-05 14:41:53 -0400

    CSS: add text color

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -5,6 +5,11 @@ class ParsersController < ApplicationController
 
     def index 
         @events = Event.all
+        all_comments = Comment.all
+        @comments = all_comments.each do |comments|
+            comments
+        end
+        # binding.pry
     end
       
 
