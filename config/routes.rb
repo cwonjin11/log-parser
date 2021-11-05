3664686a71ddc08bf51ec95aa1c1c5838be4702d
Wonjin Cho <cwonjin1@gmail.com>
2021-11-05 14:08:04 -0400

    Remaining nested routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "events#index"
-  
+
   resources :events do
     resources :comments
   end
+  # resources :comments
+
   resources :parsers
 
 end
