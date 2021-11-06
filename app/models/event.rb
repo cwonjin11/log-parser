4a74c885222c4f2b76a0f4b38a656fd9f91cdd78
Wonjin Cho <cwonjin1@gmail.com>
2021-11-06 01:13:32 -0400

    Add validation for creating log data and show error messages if invalid

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,10 @@ class Event< ApplicationRecord
     # dependent: :destroy means the comments related
     #to the specific post in mention get deleted if the post does
     
+    validates_presence_of :data
+    validates_format_of :data, with: /src=((\d{1,3}\.){3}\d{1,3})/, on: :create, message: "must include Source IP address"
+    validates_format_of :data, with: /dst=((\d{1,3}\.){3}\d{1,3})/, on: :create, message: "must include Destination ip address"
+    
     # log creation time formatting
     def datetime
         self.created_at.strftime("%Y-%m-%d / %H:%M:%S") if self.created_at
