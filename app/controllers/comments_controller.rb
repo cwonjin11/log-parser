446e6fc6385691f6ff6fa92036a1d2066f2f19ce
Wonjin Cho <cwonjin1@gmail.com>
2021-11-05 21:58:07 -0400

    Refactor : before action find_comment, find_event, find_event_comment added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,33 @@
 class CommentsController < ApplicationController
+    before_action :find_comment, only: [:show, :edit]
+    before_action :find_event, only: [:new, :create, :edit, :update, :destroy]
+    before_action :find_event_comments, only: [:update, :destroy]
 
     def index
         @comments = Comment.all
     end
 
     def show
-        @comment = Comment.find(params[:id])
+        
     end
 
     def new
-        @event = Event.find(params[:event_id])
         @comment = @event.comments.build
     end
 
     
     def create
-        @event = Event.find(params[:event_id])
         @comment = @event.comments.create(comment_params)
         redirect_to event_path(@event)   
     end
     
     
     def edit
-        @comment= Comment.find(params[:id])
-        @event = Event.find(params[:event_id])
+
     end
     
     
     def update
-        @event = Event.find(params[:event_id])
-        @comment = @event.comments.find(params[:id])
         if @comment.update(comment_params)
             flash[:message] = "Comment has been updated!"
             redirect_to event_path(@event)
@@ -40,8 +38,6 @@ class CommentsController < ApplicationController
     
     
     def destroy
-        @event = Event.find(params[:event_id])
-        @comment = @event.comments.find(params[:id])
         @comment.destroy
         flash[:message] = "Comment has been deleted!"
         redirect_to event_path(@event)
@@ -52,8 +48,20 @@ class CommentsController < ApplicationController
 
     private
     
+    def find_comment
+        @comment = Comment.find(params[:id])
+    end
+
+    def find_event
+        @event = Event.find(params[:event_id])
+    end
+
+    def find_event_comments
+        @comment = @event.comments.find(params[:id])
+    end
+
     def comment_params
-      params.require(:comment).permit(:comment, :event_id)
+        params.require(:comment).permit(:comment, :event_id)
     end
 
 
